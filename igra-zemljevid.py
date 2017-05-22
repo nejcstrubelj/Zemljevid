9968f88e2530b4b59319c9c420da17fcca18cfd2
Nejc Strubelj <nejc_strubelj@hotmail.com>
2017-05-22 18:25:30 +0200

    prva ura

---
+++ igra-zemljevid.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+
+VISINA = 640
+SIRINA = 800
+
+
+
+class Zemljevid:
+    def __init__(self, okno):
+        self.okno = okno
+        self.platno = tk.Canvas(okno,
+            width = SIRINA,
+            height = VISINA
+        )
+        self.platno.grid(row = 1, column = 1, columnspan = 2)
+        self.slika = tk.PhotoImage(file = "slike držav/islandija.gif")
+        self.platno.create_image(SIRINA // 2, VISINA // 2, image = self.slika)
+
+        gumb1 = tk.Button(okno, text='1. država')
+        gumb1.grid(row=2, column=1)
+
+        gumb2 = tk.Button(okno, text='2. država')
+        gumb2.grid(row=2, column=2)
+
+        gumb2 = tk.Button(okno, text='3. država')
+        gumb2.grid(row=3, column=1)
+
+        gumb2 = tk.Button(okno, text='4. država')
+        gumb2.grid(row=3, column=2)
+
+    def naslednje_vprasanje(self):
+
+
+
+okno = tk.Tk()
+aplikacija = Zemljevid(okno)
+okno.mainloop()
+
+# funkcija "naslednje_vprasanje(self):", se bo sprožila takoj ko boš odprl aplikacijo.
+
+#kar bos naslednje uporabil je funkcija "listdir(path='.')", (profesor jo je našel
+# na naslovu https://docs.python.org/3/library/os.html) s tem bos nastel vsa imena datotek
+#(slik držav) v neki mapi. Nato bosš iz teh imen naključno izbral eno od njih in jo odprl.
+#nato bos to ime države se napisal na naključno izbran gumb, na ostale tri gumbe pa napisal neka
+#druga 3 nakljucna imena drzav s tistega seznama.
